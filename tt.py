0554b1bb57160157476ac71f7a21905885a9660f
matlob <101573299+matlob@users.noreply.github.com>
2022-04-17 07:52:04 +0200

    Update tt.py

--- tt.py
+++ tt.py
@@ -1,17 +1,28 @@
-from telethon.events import StopPropagation
-from telethon.sync import TelegramClient, events
-import asyncio
-import time
+from telethon import TelegramClient, events, sync
+import re
+from time import sleep
+apd = 5706121
+aph = 'feaa35ddd9bf15e761de280218c9d7fd'
 
-api_id = 14475649
-api_hash = "8a92afffeb0571bbe1a9fa4e91087e6d"
-Done_Coins = 0
-Group = "kllllllwowo"
-with TelegramClient('session', api_id, api_hash) as client:
-  client.send_message(Group, "راتب")
-  client.send_message(Group, "بخشيش")
-  for i in range(2000):
-     time.sleep(600)
-     client.send_message(Group, "راتب")
-     time.sleep(0.2)
-     client.send_message(Group, "بخشيش")
+
+channel_username = "kllllllwowo"
+def sas():
+	app =  TelegramClient('session_name', api_id=apd,api_hash=aph)
+	app.start()
+	for i in range(591):
+		app.send_message(channel_username,"فلوس")
+		sleep(1)
+		for message in app.get_messages(channel_username, limit=4):
+			try:
+				for message in app.get_messages(channel_username, limit=10):
+					
+					aaa = re.findall(r'\d+', message.message)
+					
+					print(aaa)
+					sleep(1)
+					app.send_message(channel_username,f"استثمار {aaa[0]}")
+					app.send_message(channel_username,"جيد")
+					sleep(3000)
+			except:
+				pass
+sas()
